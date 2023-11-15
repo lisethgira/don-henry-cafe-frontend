6bcec349027ba122ecedb6c06735d0c94779c9f5
Liseth Giraldo <liseth.giraldo@rocketfy.co>
2023-11-15 01:54:35 -0500

    fix: se ajusta navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,93 @@
+import { Link, NavLink } from "react-router-dom";
+
+//imagenes
+import logo from "../assets/donHenryCafe.png";
+
 const Navbar = () => {
   return (
     <>
       <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <nav
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+               <li>
+              <NavLink to="/"  className={({ isActive }) =>
+                    isActive ? "font-bold text-[#6A4029]" : ""
+                  }>
+                <a>Home</a>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/productos"  className={({ isActive }) =>
+                    isActive ? "font-bold text-[#6A4029]" : ""
+                  }>
+                <a>Productos</a>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/eventos"  className={({ isActive }) =>
+                    isActive ? "font-bold text-[#6A4029]" : ""
+                  }>
+                <a>Eventos</a>
+              </NavLink>
+            </li>
+            </nav>
+          </div>
+          <Link to="/">
+            <a className="btn btn-ghost">
+            <img src={logo} alt="logo" width="30px" />
+            <p className="hidden md:flex text-xl">Don Henry Café</p>
+            </a>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <nav className="menu menu-horizontal px-1">
+            <li>
+              <NavLink to="/"  className={({ isActive }) =>
+                    isActive ? "font-bold text-[#6A4029]" : ""
+                  }>
+                <a>Home</a>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/productos"  className={({ isActive }) =>
+                    isActive ? "font-bold text-[#6A4029]" : ""
+                  }>
+                <a>Productos</a>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/eventos"  className={({ isActive }) =>
+                    isActive ? "font-bold text-[#6A4029]" : ""
+                  }>
+                <a>Eventos</a>
+              </NavLink>
+            </li>
+          </nav>
+        </div>
+
+        <div className="navbar-end gap-2">
+          <a className="btn">Iniciar Sesión</a>
+          <a className="btn">Registrarse</a>
         </div>
         <div className="flex-none">
           <div className="dropdown dropdown-end">
@@ -32,10 +116,10 @@ const Navbar = () => {
             >
               <div className="card-body">
                 <span className="font-bold text-lg">8 Items</span>
-                <span className="text-info">Subtotal: $999</span>
+                <span className="text-info">Subtotal: $10.000</span>
                 <div className="card-actions">
                   <button className="btn btn-primary btn-block">
-                    View cart
+                    Ver Carrito
                   </button>
                 </div>
               </div>
@@ -56,15 +140,14 @@ const Navbar = () => {
             >
               <li>
                 <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
+                  Perfil
                 </a>
               </li>
               <li>
-                <a>Settings</a>
+                <a>Configuración</a>
               </li>
               <li>
-                <a>Logout</a>
+                <a>Cerrar Sesión</a>
               </li>
             </ul>
           </div>
