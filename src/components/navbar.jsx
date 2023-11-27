bce7d5c645e3048c7e19e669626f429d2ddb4cbc
Liseth Giraldo <liseth.giraldo@rocketfy.co>
2023-11-27 05:01:32 -0500

    fix(home) se ajusta home

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
                     isActive ? "font-bold text-[#6A4029]" : ""
                   }
                 >
-                  <a>Home</a>
+                  Home
                 </NavLink>
               </li>
               <li>
@@ -70,7 +70,7 @@ const Navbar = () => {
                     isActive ? "font-bold text-[#6A4029]" : ""
                   }
                 >
-                  <a>Productos</a>
+                  Productos
                 </NavLink>
               </li>
               <li>
@@ -80,16 +80,14 @@ const Navbar = () => {
                     isActive ? "font-bold text-[#6A4029]" : ""
                   }
                 >
-                  <a>Eventos</a>
+                  Eventos
                 </NavLink>
               </li>
             </nav>
           </div>
-          <Link to="/">
-            <a className="btn btn-ghost">
-              <img src={logo} alt="logo" width="30px" />
-              <p className="hidden md:flex text-xl">Don Henry Café</p>
-            </a>
+          <Link to="/" className="btn btn-ghost">
+            <img src={logo} alt="logo" width="30px" />
+            <p className="hidden md:flex text-xl">Don Henry Café</p>
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
@@ -101,7 +99,7 @@ const Navbar = () => {
                   isActive ? "font-bold text-[#6A4029]" : ""
                 }
               >
-                <a>Home</a>
+                Home
               </NavLink>
             </li>
             <li>
@@ -111,7 +109,7 @@ const Navbar = () => {
                   isActive ? "font-bold text-[#6A4029]" : ""
                 }
               >
-                <a>Productos</a>
+                Productos
               </NavLink>
             </li>
             <li>
@@ -121,88 +119,88 @@ const Navbar = () => {
                   isActive ? "font-bold text-[#6A4029]" : ""
                 }
               >
-                <a>Eventos</a>
+                Eventos
               </NavLink>
             </li>
           </nav>
         </div>
         <div className="navbar-end gap-2">
-        {!token ? (
-           <div className="flex gap-2">
-            <Link to="/login" className="btn bg-secondary text-tertiary">
-              Iniciar Sesión
-            </Link>
-            <Link to="/register" className="btn bg-secondary text-tertiary">
-              Registrarse
-            </Link>
-            </div>
-        ) : null}
-
-        <div className="flex-none">
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
-                <span className="badge badge-sm indicator-item">
-                  {quantity}
-                </span>
-              </div>
-            </label>
-            <div
-              tabIndex={0}
-              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
-            >
-              <div className="card-body">
-                <span className="font-bold text-lg"> {quantity} Items</span>
-                <span className="text-info">Subtotal: ${subTotal} </span>
-                <div className="card-actions">
-                  <Link to="/cart" className="btn btn-primary btn-block">
-                    Ver Carrito
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-          {token ? (
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <img
-                    alt="Tailwind CSS Navbar component"
-                    src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/44/44dc4bee8bf175f573c7d417afc31540ac40f518_full.jpg"
-                  />
-                </div>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-              >
-                <li>
-                  <a className="justify-between">Perfil</a>
-                </li>
-                <li>
-                  <a>Configuración</a>
-                </li>
-                <li onClick={cerrarSesionRef}>
-                  <a>Cerrar Sesión</a>
-                </li>
-              </ul>
+          {!token ? (
+            <div className="flex gap-2">
+              <Link to="/login" className="btn bg-secondary text-tertiary">
+                Iniciar Sesión
+              </Link>
+              <Link to="/register" className="btn bg-secondary text-tertiary">
+                Registrarse
+              </Link>
             </div>
           ) : null}
-        </div>
+
+          <div className="flex-none">
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle">
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+                  <span className="badge badge-sm indicator-item">
+                    {quantity}
+                  </span>
+                </div>
+              </label>
+              <div
+                tabIndex={0}
+                className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+              >
+                <div className="card-body">
+                  <span className="font-bold text-lg"> {quantity} Items</span>
+                  <span className="text-info">Subtotal: ${subTotal} </span>
+                  <div className="card-actions">
+                    <Link to="/cart" className="btn btn-primary btn-block">
+                      Ver Carrito
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {token ? (
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                    <img
+                      alt="Tailwind CSS Navbar component"
+                      src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/44/44dc4bee8bf175f573c7d417afc31540ac40f518_full.jpg"
+                    />
+                  </div>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <a className="justify-between">Perfil</a>
+                  </li>
+                  <li>
+                    <a>Configuración</a>
+                  </li>
+                  <li onClick={cerrarSesionRef}>
+                    <a>Cerrar Sesión</a>
+                  </li>
+                </ul>
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
     </>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,62 +1,134 @@
-const Home =() => {
+// import { useState, useMapEvents } from "react";
 
-return (
+import { Link } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+// imagenes
+import cliente from "../../assets/images/cliente.jpeg";
+import logo from "../../assets/images/logo.png";
+
+//video
+import video from "../../assets/videos/henrrycafe.mp4";
+
+const Home = () => {
+  const position = [6.233329974888954, -75.6041478181902];
+
+  return (
     <>
-    <main>
-    <div className="min-h-screen flex justify-center items-center bg-gray-100 py-6">
-        <div className="container mx-6">
-          <h1 className="text-4xl font-bold text-center my-4 text-gray-800">
-            Bienvenido a Don Henry Café
-          </h1>
-          <p className="hover-bg text-lg lg:text-xl text-gray-800 leading-relaxed mb-8 px-6 lg:px-12 py-8 bg-white rounded-lg shadow-md">
-            <span className="block text-3xl lg:text-4xl font-bold text-gray-900 mb-4">
-            </span>
-            <p className="mb-4">
-              Sumérgete en nuestro mundo y descubre una amplia gama de cafés de origen único, cuidadosamente seleccionados de las mejores cosechas de Colombia. Desde los exuberantes paisajes montañosos hasta las manos expertas que cultivan y procesan estos granos, cada taza de café que servimos cuenta una historia de pasión y calidad inigualable.
+      <div className="flex flex-col">
+        {/* Sección Hero */}
+        <section className="pt-20 pb-6 bg-gray-100">
+          <div className="container mx-auto text-center">
+            <h1 className="text-5xl font-bold text-gray-900 mb-6">
+              Bienvenido a Don Henry Café
+            </h1>
+            <p className="text-2xl text-gray-800">
+              Café colombiano de calidad superior, en un acogedor espacio para
+              amantes del buen café.
             </p>
-            <p className="mb-4">
-              En Don Henry Café, no solo te ofrecemos una experiencia de degustación excepcional, sino que también te invitamos a explorar y aprender sobre el arte detrás de cada taza. Nuestros expertos baristas te guiarán a través del proceso de elaboración, desde la cosecha hasta la preparación, revelando los secretos que hacen que nuestros cafés sean únicos en sabor y aroma.
-            </p>
-            <p className="mb-4">
-              Además de deleitarte con nuestra selección exclusiva de cafés, podrás disfrutar de un ambiente acogedor y relajante que complementa a la perfección la experiencia sensorial que ofrecemos. Desde los momentos íntimos de la primera taza del día hasta las conversaciones animadas en una tarde soleada, Don Henry Café es el lugar donde los apasionados del café encuentran su hogar.
-            </p>
-            <p>
-              Te invitamos a sumergirte en el mundo del café en Don Henry Café y a disfrutar de una experiencia que estimulará tus sentidos, alimentará tu curiosidad y te llevará en un viaje sensorial por los sabores auténticos de Colombia.
-            </p>
-          </p>
+            <Link to="/productos" className="btn btn-primary mt-6">
+              Ver Productos
+            </Link>
+          </div>
+        </section>
 
-          <div className="mt-8 bg-white p-6 rounded-lg shadow-2xl border-2 border-gray-300 relative">
-            <h2 className="text-2xl font-bold mb-4 text-yellow-900">
-              Quiénes Somos
-            </h2>
-            <p className="text-gray-700 leading-relaxed mb-4">
-              <span className="font-bold">
-                En el corazón de Medellín, en el concurrido Centro Comercial Los
-                Molinos, se encuentra Don Henry Café, un rincón acogedor que
-                destaca entre la multitud. La historia de este lugar tan
-                especial está entrelazada con la determinación y la pasión de su
-                fundador, Henry González.
-              </span>
-              {/* Resto de la historia aquí */}
-            </p>
-            <div
-              className="mt-4 relative rounded-lg overflow-hidden"
-              style={{
-                paddingBottom: '56.25%', // 16:9 aspect ratio for responsive video
-              }}
-            >
-              <video
-                src="public\videos\henrrycafe.mp4"
-                loop
-                controls
-                autoPlay
-                muted
-                className="absolute w-full h-full"
-              ></video>
+        {/* Sección Quiénes Somos */}
+        <section className="py-20 bg-white">
+          <div className="container mx-auto">
+            <div className="text-center mb-8">
+              <h2 className="text-4xl font-bold mb-4 text-primary">
+                Quiénes Somos
+              </h2>
+              <p className="text-gray-500">Conoce nuestra historia</p>
+            </div>
+
+            <div className="md:flex gap-10">
+              <div className="mb-6 md:mb-0 md:w-1/2 text-justify">
+                <span className="block text-3xl lg:text-4xl font-bold text-gray-900 mb-4"></span>
+                <p className="mb-4">
+                  {" "}
+                  Sumérgete en nuestro mundo y descubre una amplia gama de cafés
+                  de origen único, cuidadosamente seleccionados de las mejores
+                  cosechas de Colombia. Desde los exuberantes paisajes
+                  montañosos hasta las manos expertas que cultivan y procesan
+                  estos granos, cada taza de café que servimos cuenta una
+                  historia de pasión y calidad inigualable.
+                </p>
+
+                <p className="mb-4">
+                  En Don Henry Café, no solo te ofrecemos una experiencia de
+                  degustación excepcional, sino que también te invitamos a
+                  explorar y aprender sobre el arte detrás de cada taza.
+                  Nuestros expertos baristas te guiarán a través del proceso de
+                  elaboración, desde la cosecha hasta la preparación, revelando
+                  los secretos que hacen que nuestros cafés sean únicos en sabor
+                  y aroma.
+                </p>
+
+                <p className="mb-4">
+                  Además de deleitarte con nuestra selección exclusiva de cafés,
+                  podrás disfrutar de un ambiente acogedor y relajante que
+                  complementa a la perfección la experiencia sensorial que
+                  ofrecemos. Desde los momentos íntimos de la primera taza del
+                  día hasta las conversaciones animadas en una tarde soleada,
+                  Don Henry Café es el lugar donde los apasionados del café
+                  encuentran su hogar.
+                </p>
+
+                <p>
+                  Te invitamos a sumergirte en el mundo del café en Don Henry
+                  Café y a disfrutar de una experiencia que estimulará tus
+                  sentidos, alimentará tu curiosidad y te llevará en un viaje
+                  sensorial por los sabores auténticos de Colombia.
+                </p>
+
+                <p className="text-gray-700 leading-relaxed mt-8">
+                  <span className="font-bold">
+                    En el corazón de Medellín, en el concurrido Centro Comercial
+                    Los Molinos, se encuentra Don Henry Café, un rincón acogedor
+                    que destaca entre la multitud. La historia de este lugar tan
+                    especial está entrelazada con la determinación y la pasión
+                    de su fundador, Henry González.
+                  </span>
+                  {/* Resto de la historia aquí */}
+                </p>
+              </div>
+
+              <div className="md:w-1/2">
+                <div className="relative h-64 overflow-hidden rounded-lg mb-6">
+                  <img
+                    src={cliente}
+                    alt="Imagen café"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+
+                <div
+                  className="relative overflow-hidden w-full"
+                  style={{ height: "500px", width: "750px" }}
+                >
+                  <img src={logo} alt="Vista previa del video" />
+                  <video
+                    className="absolute top-0 left-0 w-full h-full"
+                    controls
+                    muted
+                  >
+                    <source src={video} type="video/mp4" />
+                  </video>
+                </div>
+              </div>
+            </div>
+
+            <div className="text-center pt-16">
+              <Link to="/about" className="btn btn-primary">
+                Conocer Más
+              </Link>
             </div>
           </div>
+        </section>
 
-          {/* Sección de comentarios */}
+        {/* Sección Comentarios */}
+        <section className="my-8">
           <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-4">
             {Array.from({ length: 3 }).map((_, index) => (
               <div key={index} className="bg-gray-200 p-4 rounded-lg">
@@ -79,8 +151,9 @@ return (
                       key={starIndex}
                       role="img"
                       aria-label="star"
-                      className={`text-yellow-400 ${starIndex < 4 ? 'mr-1' : ''
-                        }`}
+                      className={`text-yellow-400 ${
+                        starIndex < 4 ? "mr-1" : ""
+                      }`}
                     >
                       ⭐️
                     </span>
@@ -89,11 +162,50 @@ return (
               </div>
             ))}
           </div>
-        </div>
+        </section>
+
+        {/* Sección Mapa */}
+        <section>
+          <MapContainer
+            center={position}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{
+              height: "400px",
+              width: "100%",
+              margin: "10px 0px 50px 0px",
+              backgroundColor: "lightgray",
+            }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                <div className="text-center">
+                  <h2 className="mb-2 text-lg font-bold">Don Henry Café</h2>
+                  <p className="mb-1">
+                    <strong>Ubicación:</strong> Centro Comercial los Molinos,Piso 8
+                  </p>
+                  <p className="mb-1">
+                    <strong>Dirección:</strong> Cl. 30A #82A-26, Medellín,
+                    Belén, Medellín, Antioquia
+                  </p>
+                  <p className="mb-1">
+                    <strong>Contacto:</strong> 300 5710804
+                  </p>
+                  <p className="mb-1">
+                    <strong>Correo:</strong> infotintico@gmail.com
+                  </p>
+                </div>
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </section>
       </div>
-    </main>
     </>
-);
-}
+  );
+};
 
 export default Home;
